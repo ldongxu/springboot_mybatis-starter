594ed19816a16593a11941e3d1ecb1e3b454665c
ldongxu <ldongxu@126.com>
2022-02-24 11:06:16 +0000

    update src/test/java/com/example/demo/DemoApplicationTests.java.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.dao.ProxyWhitelistConfDao;
 import com.example.demo.dao.ProxyWhitelistMapper;
 import com.example.demo.entity.ProxyWhitelist;
 import org.junit.jupiter.api.Test;
@@ -14,26 +13,13 @@ class DemoApplicationTests {
 
     @Autowired
     private ProxyWhitelistMapper proxyWhitelistMapper;
-    @Resource
-    private ProxyWhitelistConfDao proxyWhitelistConfDao;
+    
 
     @Test
     void contextLoads() {
         ProxyWhitelist en = new ProxyWhitelist();
         en.setHost("a.b.com");
         proxyWhitelistMapper.insertSelective(en);
-    }
-
-    @Test
-    void testDao(){
-        int a = proxyWhitelistConfDao.countListByHostAndDate(null,"2022-01-24 18:39:37.458","2022-02-24 18:39:37.458");
-        System.out.println(a);
-    }
-
-    @Test
-    void testInsert(){
-        Long a = proxyWhitelistConfDao.insert("aaa.com","aaaaaaaaa");
-        System.out.println(a);
     }
 
 }
